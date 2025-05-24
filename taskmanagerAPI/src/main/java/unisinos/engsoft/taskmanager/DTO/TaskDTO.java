a818d4717a7746162f72a37efac3ec08159945c2
arthur-palma <arthur.p.palma@gmail.com>
2025-05-24 20:37:08 -0300

    ref - adjusting Task errors, TaskDTO, adding Task Mapper

--- taskmanagerAPI/src/main/java/unisinos/engsoft/taskmanager/DTO/TaskDTO.java
+++ taskmanagerAPI/src/main/java/unisinos/engsoft/taskmanager/DTO/TaskDTO.java
@@ -6,6 +6,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 
 public class TaskDTO {
 
@@ -13,6 +14,6 @@ public class TaskDTO {
     private String title;
     private String description;
     private String status;
-    private int assignedTo;
+    private UserDTO assignedTo;
 
 }
